8b2091efe2150d595b567061d07cef89be58dffe
Akinlade Feranmi <feranmiakinlade@gmail.com>
2019-10-29 16:34:35 +0100

    Finish email sending logic

--- src/routes/email.js
+++ src/routes/email.js
@@ -1,28 +1,33 @@
 var nodemailer = require('nodemailer');
+import { EMAIL_ADDR, EMAIL_PASSWORD } from '../config/constants';
 
-res.render('list.ejs', {posts: posts}, function(err, list){
-  // 
-});
+export function sendMail(recipientAddr, title, body) {
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: EMAIL_ADDR,
+      pass: EMAIL_PASSWORD
+    }
+  });
+  
+  var mailOptions = {
+    from: `ITARJ Notifications <${EMAIL_ADDR}>`,
+    to: recipientAddr,
+    subject: title,
+    text: body
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+}
 
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'feranmiakinlade@gmail.com',
-    pass: 'af.27111998'
-  }
-});
-
-var mailOptions = {
-  from: 'feranmiakinlade@gmail.com',
-  to: 'feranmiakinlade@outlook.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
+export function notifyByEmail(res, params) {
+  res.render('notif.ejs', params, function(err, renderedEmail) {
+    sendMail(recipientAddr, title, renderedEmail);
+  });
+}
